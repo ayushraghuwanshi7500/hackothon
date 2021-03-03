397a5effad8cba8972c63ed2197ef2c09464c297
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-03-03 10:45:46 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('<h1>Server Running</h1>'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
